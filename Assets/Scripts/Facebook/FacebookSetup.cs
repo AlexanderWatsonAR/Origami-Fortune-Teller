61ab01d7f60e1975ba8bdc653c7f90d4711528cf
Alexander Watson <msnalexwatson@hotmail.co.uk>
2021-11-15 13:35:51 +0000

    Facebook SDK
    app with facebook sdk installed

---
+++ Assets/Scripts/Facebook/FacebookSetup.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+
+
+public class FacebookSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                    FB.ActivateApp();
+                else
+                    Debug.LogError("Couldn't initialize");
+            },
+
+            isGameShown =>
+            {
+                if (!isGameShown)
+                    Time.timeScale = 0;
+                else
+                    Time.timeScale = 1;
+            });
+        }
+        else
+            FB.ActivateApp();
+    }
+}
+
+
+
